81a678de8de22da28e32236835152f8d0492b3fd
Alexandra Kostrova <sashabravos@gmail.com>
2023-02-27 23:16:27 +0300

    add all files

---
+++ yourWay/Logic/Story.swift
@@ -0,0 +1,16 @@
+//
+//  Story.swift
+//  yourWay
+//
+//  Created by Александра Кострова on 27.02.2023.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
